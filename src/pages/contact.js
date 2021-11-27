8f6a73e3724468a50e609f31f2956a1c92483b2e
Daniel Weber <1678401@park.edu>
2021-11-27 01:27:58 -0600

    added hamburger menu icon

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,6 +6,7 @@ const Contact = () => {
     <Layout>
       <main className="page">
         <section className="contact-page">
+          {/* ****Contact Info**** */}
           <article className="contact-info">
             <h3>want to get in touch?</h3>
             <p>
@@ -18,20 +19,25 @@ const Contact = () => {
             </p>
             <p>Don't be shy contact us now!</p>
           </article>
+          {/* ****Form**** */}
           <article>
             <form className="form contact-form">
+              {/* **Name** */}
               <div className="form-row">
                 <label htmlFor="name">Your Name</label>{" "}
                 <input type="text" name="name" id="name" />
               </div>
+              {/* **Email** */}
               <div className="form-row">
                 <label htmlFor="email">Your Email</label>{" "}
                 <input type="text" name="email" id="email" />
               </div>
+              {/* **Message** */}
               <div className="form-row">
                 <label htmlFor="message">Message</label>{" "}
                 <input type="textarea" name="message" id="message" />
               </div>
+              {/* **Submit** */}
               <button className="btn block">Submit</button>
             </form>
           </article>
