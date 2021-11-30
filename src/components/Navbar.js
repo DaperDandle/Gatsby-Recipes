7ad8f7fd19a64517cde368cdb4b792efb645e241
Daniel Weber <1678401@park.edu>
2021-11-30 00:11:23 -0600

    Finished contact page, added navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,46 @@
 import React from "react"
 import { Link } from "gatsby"
 import { FiAlignJustify } from "react-icons/fi"
+import logo from "../assets/images/logo.svg"
+import { useState } from "react"
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false)
   return (
-    <nav>
-      <FiAlignJustify />
-      <ul>
-        <li>
-          <Link to="/">home</Link>
-        </li>
-        <li>
-          <Link to="/about">about</Link>
-        </li>
-        <li>
-          <Link to="/contact">contact</Link>
-        </li>
-        <li>
-          <Link to="/tags">tags</Link>
-        </li>
-        <li>
-          <Link to="/recipes">recipes</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="simply recipes" />
+          </Link>
+          <button className="nav-btn" onClick={() => setShowLinks(!showLinks)}>
+            <FiAlignJustify />
+          </button>
+        </div>
+        <div className={showLinks ? "nav-links show-links" : "nav-links"}>
+          <Link to="/" className="nav-link" activeClassName="active-link">
+            home
+          </Link>
+          <Link
+            to="/recipes"
+            className="nav-link"
+            activeClassName="active-link"
+          >
+            recipes
+          </Link>
+          <Link to="/tags" className="nav-link" activeClassName="active-link">
+            tags
+          </Link>
+          <Link to="/about" className="nav-link" activeClassName="active-link">
+            about
+          </Link>
+          <div className="nav-link contact-link">
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
