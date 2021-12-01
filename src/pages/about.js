cd4dfc7ce705458cf646c656eda1212d0037d9b6
Daniel Weber <1678401@park.edu>
2021-12-01 00:15:18 -0600

    Finished about page, added hero image and text to home page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,34 @@
 import React from "react"
 import Layout from "../components/Layout"
-
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 const About = () => {
   return (
     <Layout>
-      <h1>about</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>About Us</h2>
+            <p>
+              Simply Recipes was created in 2021 to bring delicious recipes to
+              the masses.
+            </p>
+            <p>
+              Everyone should know how to cook for themselves, check out some of
+              our ideas for interesting meals for any dinner!
+            </p>
+            <Link to="/contact" className="btn">
+              Contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="person pouring salt in bowl"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
